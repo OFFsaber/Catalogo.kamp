396961de0df4d4b2ebadd92082d5537de355334f
Samuel Else <samuelelse2@gmail.com>
2025-05-09 15:02:09 -0300

    excluiri arquivos desnecessarios

--- configurar_git_simples.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setx PATH "%PATH%;C:\Program Files\Git\bin;C:\Program Files\Git\cmd" /M
-echo Git configurado com sucesso!
-echo Por favor, reinicie o PowerShell para que as alteracoes tenham efeito.
-pause 
